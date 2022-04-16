4fbc333d4e0ff998e7f2d4ac92a55c7b4cc75373
unknown <orishameedah@gmail.com>
2022-04-16 02:25:30 -0700

    Add Expense Styles

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -57,10 +57,10 @@ export default class ExpenseForm extends React.Component {
   };
   render() {
     return (
-      <div>
-        {this.state.error && <p>{this.state.error}</p>}
-        <form onSubmit={this.onSubmit}>
+        <form className="form" onSubmit={this.onSubmit}>
+          {this.state.error && <p className="form__error">{this.state.error}</p>}
           <input
+            className="text-input"
             type="text"
             placeholder="Description"
             autoFocus
@@ -68,7 +68,8 @@ export default class ExpenseForm extends React.Component {
             onChange={this.onDescriptionChange}
           />
           <input
-            type="text"
+          className="text-input"
+          type="text"
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
@@ -82,13 +83,13 @@ export default class ExpenseForm extends React.Component {
             isOutsideRange={() => false}
           />
           <textarea
+           className="textarea"
             placeholder="Add a note for your expense (optional)"
             value={this.state.note}
             onChange={this.onNoteChange}
           ></textarea>
-          <button>Add Expenses</button>
+          <button className="button">Save Expense</button>
         </form>
-      </div>
     );
   }
 }
